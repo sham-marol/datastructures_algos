850d6fcb44290f246f8e7a1bc9862eaa22f171a0
sham-marol <sham.marol@gmail.com>
2023-10-10 12:59:20 -0400

    Create reverse_bits.py

---
+++ reverse_bits.py
@@ -0,0 +1,43 @@
+"""
+Reverse bits of a given 32 bits unsigned integer.
+
+Note:
+
+Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+ 
+Example 1:
+
+Input: n = 00000010100101000001111010011100
+Output:    964176192 (00111001011110000010100101000000)
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+Example 2:
+
+Input: n = 11111111111111111111111111111101
+Output:   3221225471 (10111111111111111111111111111111)
+Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+ 
+
+Constraints:
+
+The input must be a binary string of length 32
+
+-------------------------
+
+Runtime : 67 ms , Memory : 13.9 MB
+@author : sham-marol
+
+"""
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+                
+        rvs_bn_lst = list(reversed(str(bin(n)).replace('0b','')))        
+        req_len = 32 - len(rvs_bn_lst)
+        for i in range(req_len):
+            rvs_bn_lst.append('0')
+       
+        rvs_bn_str = "".join(rvs_bn_lst)
+        rvs_bn_dec = int(rvs_bn_str,2)
+        
+        return rvs_bn_dec
