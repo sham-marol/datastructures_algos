9186d3a628abc184d41a8ae44d2ec3c5bdb2522f
sham-marol <sham.marol@gmail.com>
2023-10-11 15:04:56 -0400

    Create fizz buzz.py

---
+++ fizz buzz.py
@@ -0,0 +1,54 @@
+"""
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+Constraints:
+
+1 <= n <= 104
+
+-------------------------
+
+Runtime : 101 ms , Memory : 14.9 MB
+@author : sham-marol
+
+"""   
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+
+        strarr = []
+        for i in range(1,n+1,1):            
+            
+            if (i % 3 == 0) and (i % 5 == 0):
+                val = "FizzBuzz"
+            elif (i % 3 == 0):
+                val = "Fizz"
+            elif (i % 5 == 0):
+                val = "Buzz"
+            else:
+                val = str(i)
+                
+            strarr.append(str(val))
+            
+        return strarr
+
